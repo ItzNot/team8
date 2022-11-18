89402622b86dc832d5443d153ae6fc8cf0b0dd7b
shindongho07 <sdh970708@naver.com>
2022-11-18 17:58:51 +0900

    복구/ 포트수정

--- Project-002/src/main/java/util/DBManager.java
+++ Project-002/src/main/java/util/DBManager.java
@@ -7,8 +7,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class DBManager {
-//sdh) 개인포트 1522로 사용중/ 개인 조건에 맞게 수정 요망 
-	private static String url = "jdbc:oracle:thin:@localhost:1522:orcl";
+	private static String url = "jdbc:oracle:thin:@localhost:1521:orcl";
 	private static String uid = "scott";
 	private static String pwd = "tiger";
 
