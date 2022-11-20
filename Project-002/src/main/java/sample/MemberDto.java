bf731bf7f218186626ce2bdc30781aef59d88244
ItzNot <45924844+ItzNot@users.noreply.github.com>
2022-11-20 22:09:25 +0900

    검색기능구현

---
+++ Project-002/src/main/java/sample/MemberDto.java
@@ -0,0 +1,89 @@
+package sample;
+
+import java.sql.Date;
+
+public class MemberDto {
+	String id;
+	String pwd;
+	String name;
+	String email;
+	String zip_num;
+	String address;
+	String phone;
+	String useyn;
+	Date indate;
+	int allPrice;
+	int memAllPrice;
+	public int getMemAllPrice() {
+		return memAllPrice;
+	}
+	public void setMemAllPrice(int memAllPrice) {
+		this.memAllPrice = memAllPrice;
+	}
+	public int getAllPrice() {
+		return allPrice;
+	}
+	public void setAllPrice(int allPrice) {
+		this.allPrice = allPrice;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getZip_num() {
+		return zip_num;
+	}
+	public void setZip_num(String zip_num) {
+		this.zip_num = zip_num;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getUseyn() {
+		return useyn;
+	}
+	public void setUseyn(String useyn) {
+		this.useyn = useyn;
+	}
+	public Date getIndate() {
+		return indate;
+	}
+	public void setIndate(Date indate) {
+		this.indate = indate;
+	}
+	@Override
+	public String toString() {
+		return "MemberDto [id=" + id + ", pwd=" + pwd + ", name=" + name + ", email=" + email + ", zip_num=" + zip_num
+				+ ", address=" + address + ", phone=" + phone + ", useyn=" + useyn + ", indate=" + indate
+				+ ", allPrice=" + allPrice + ", memAllPrice=" + memAllPrice + "]";
+	}
+}
